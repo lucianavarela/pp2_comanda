110889b5c88d732f126fb786e95f0bce3ff38544
lucianavarela <lucianavarela@hotmail.es>
2019-06-17 18:14:24 -0300

    empleado

---
+++ src/app/components/abm-empleado/abm-empleado.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abm-empleado',
+  templateUrl: './abm-empleado.component.html',
+  styleUrls: ['./abm-empleado.component.scss'],
+})
+export class AbmEmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
