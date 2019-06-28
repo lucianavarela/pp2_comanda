458312ed4ad0f7464eaaeb9228d06b1ba582ccb1
Mauricio Cerizza <mauriciocerizza@gmail.com>
2019-06-28 04:32:01 -0300

    Calidad foto

--- src/app/services/fotos/images.service.ts
+++ src/app/services/fotos/images.service.ts
@@ -6,7 +6,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class ImagesService {
   options: CameraOptions = {
-    quality: 50,
+    quality: 10,
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE,
