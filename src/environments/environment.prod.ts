641209ba3dfde8562ab11026c7bee18960954a7e
Your Name <you@example.com>
2019-06-29 12:19:52 -0300

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,12 @@ export const environment = {
   production: true
 };
 
-export const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyA4J3ygcyZa-sqUokQbA0yZtBvCRS87HaU",
   authDomain: "relevamiento-visual-9b705.firebaseapp.com",
   databaseURL: "https://relevamiento-visual-9b705.firebaseio.com",
   projectId: "relevamiento-visual-9b705",
   storageBucket: "relevamiento-visual-9b705.appspot.com",
-  messagingSenderId: "140650278758"
+  messagingSenderId: "140650278758",
+  appId: "1:140650278758:web:80ad4427eee518b0"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,14 @@ export const environment = {
   production: false,
 };
 
-export const firebaseConfig = {
+export var firebaseConfig = {
   apiKey: "AIzaSyA4J3ygcyZa-sqUokQbA0yZtBvCRS87HaU",
   authDomain: "relevamiento-visual-9b705.firebaseapp.com",
   databaseURL: "https://relevamiento-visual-9b705.firebaseio.com",
   projectId: "relevamiento-visual-9b705",
   storageBucket: "relevamiento-visual-9b705.appspot.com",
-  messagingSenderId: "140650278758"
+  messagingSenderId: "140650278758",
+  appId: "1:140650278758:web:80ad4427eee518b0"
 };
 
 /*
