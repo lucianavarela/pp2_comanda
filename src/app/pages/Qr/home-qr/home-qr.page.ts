1df0e04536d777e2ea7a2960cea46e7e6583fe3a
Gregnanin Leandro <gregnanin.leandro@gmail.com>
2019-07-13 04:22:37 -0300

    leo

--- src/app/pages/Qr/home-qr/home-qr.page.ts
+++ src/app/pages/Qr/home-qr/home-qr.page.ts
@@ -159,37 +159,7 @@ export class HomeQrPage implements OnInit {
                 })
               }
             })
-                 /*else{
-
-                  /*if (this.verificarMesaCerrada( this.usuarioOnline.mesa)) {
-             
-                     this.clienteService.CargarMesa(this.usuarioOnline).
-                       subscribe((data) => {
-                         if (data["Estado"] == "OK") {
-                           this.errorHandler.confirmationToast(data["Mensaje"]);
-                           this.servicioMesa.CambiarEstado(this.usuarioOnline.mesa, EstadosMesa.Asignada);
-                           this.volver();
-                         } else {
-                           this.errorHandler.errorToast(data["Mensaje"]);
-                           this.volver();
-                         }
-        
-                       }, (error) => {
-                         this.errorHandler.errorToast(error);
-                         this.volver();
-                       })
-                      
-                   } else {
-                     this.errorHandler.errorToast("Esta mesa no esta libre");
-                     this.volver();
-                   }
-                   
-                 }           
-    
-            });
-          }
-          });
-       */
+                 
      
       }  
      
